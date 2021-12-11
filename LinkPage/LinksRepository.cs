7a301b71d9e05ca4d34fa3902d275cd216d171d1
Will <04ea5aa4@gmail.com>
2021-12-11 13:36:44 +1300

    Add test data to match README

--- LinkPage/LinksRepository.cs
+++ LinkPage/LinksRepository.cs
@@ -1,10 +1,17 @@
 ﻿using LinkPage.Links;
+using LinkPage.Links.Classic;
+using LinkPage.Links.Shows;
 
 namespace LinkPage
 {
     public class LinksRepository
     {
         private readonly List<Link> _links = new();
+
+        public LinksRepository()
+        {
+            PopulateTestData();
+        }
 
         public LinksRepository(IEnumerable<Link> data)
         {
@@ -29,5 +36,70 @@ namespace LinkPage
 
         public IEnumerable<Link> GetLinks(int userId) => _links
             .Where(link => link.UserId == userId);
+
+        private void PopulateTestData()
+        {
+            Add(1, new ShowsLink
+            {
+                LinkId = 1,
+                UserId = 1,
+                Title = "La Bohème",
+                Shows = new List<ShowsLink.Show>
+                {
+                    new ShowsLink.Show
+                    {
+                        Id = 1,
+                        Date = new DateTime(2022, 1, 4),
+                        VenueName = "Sydney Opera House",
+                        VenueLocation = "Sydney, Australia",
+                        IsSoldOut = true,
+                        IsOnSale = false,
+                    },
+                    new ShowsLink.Show
+                    {
+                        Id = 2,
+                        Date = new DateTime(2022, 1, 1),
+                        VenueName = "The Aquarium of Western Australia",
+                        VenueLocation = "Perth, Australia",
+                        IsSoldOut = false,
+                        IsOnSale = false,
+                    },
+                    new ShowsLink.Show
+                    {
+                        Id = 2,
+                        Date = new DateTime(2022, 1, 12),
+                        VenueName = "Chinese Garden of Friendship",
+                        VenueLocation = "Sydney, Australia",
+                        IsSoldOut = false,
+                        IsOnSale = true,
+                    },
+                    new ShowsLink.Show
+                    {
+                        Id = 2,
+                        Date = new DateTime(2022, 1, 1),
+                        VenueName = "Centre Ivanhoe",
+                        VenueLocation = "Melbourne, Australia",
+                        IsSoldOut = false,
+                        IsOnSale = true,
+                    },
+                },
+            });
+
+            Add(1, new ClassicLink
+            {
+                LinkId = 2,
+                UserId = 1,
+                Title = "DuckDuckGo",
+                Url = "https://duckduckgo.com",
+            });
+
+            Add(1, new ClassicLink
+            {
+                LinkId = 3,
+                UserId = 1,
+                Title = "Signal",
+                Url = "https://signal.org",
+            });
+        }
     }
 }
--- LinkPage/Program.cs
+++ LinkPage/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddSingleton<LinksRepository>();
+builder.Services.AddSingleton<LinksRepository>(new LinksRepository());
 
 var app = builder.Build();
 
